a5312b533ac78e123f370b96311751202d612533
Casey <cmay@Caseys-MacBook-Pro.local>
2024-03-19 23:35:40 -0400

    Make progress on image UI

--- movie-mojo-frontend/src/components/MoviePoster.tsx
+++ movie-mojo-frontend/src/components/MoviePoster.tsx
@@ -10,11 +10,11 @@ const imageHeight = Math.round(dimensions.width * 0.75 * 16 / 9);
 const imageWidth = dimensions.width * 0.75;
 
 const MoviePoster: React.FC<MoviePosterProps> = ({ uri }) => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true); // Reset loading state when URI changes
-    console.log(uri)
+    // setIsLoading(true); // Reset loading state when URI changes
+    console.log(`${`https://image.tmdb.org/t/p/w500`}${uri}`)
   }, [uri]);
 
   return (
@@ -24,7 +24,7 @@ const MoviePoster: React.FC<MoviePosterProps> = ({ uri }) => {
       )}
       <Image
         // source={`${`https://image.tmdb.org/t/p/w500/`}+ ${uri}`}
-        source={{uri: `${`https://image.tmdb.org/t/p/w500/`}+ ${uri}`}}
+        source={{uri: `${`https://image.tmdb.org/t/p/w500`}+ ${uri}`}}
         className='self-center'
         style={{ height: imageHeight, width: imageWidth, opacity: isLoading ? 0 : 1, position: 'absolute', top: 0, left: 0 }}
         onLoad={() => setIsLoading(false)}
